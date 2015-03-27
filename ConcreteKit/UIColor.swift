807ecea063476b63405083383454010db71966f1
Lorenzo Villani <lorenzo@villani.me>
2015-03-27 23:09:12 +0100

    Add UIColor extension

---
+++ ConcreteKit/UIColor.swift
@@ -0,0 +1,39 @@
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Lorenzo Villani
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+import UIKit
+
+extension UIColor {
+    /// A convenience constructor to create a color from its hexadecimal representation.
+    ///
+    /// :param: hex The color.
+    /// :param: alpha The alpha value.
+    convenience init(hex: UInt32, alpha: CGFloat = 1.0) {
+        let r = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+        let g = CGFloat((hex & 0x00FF00) >> 08) / 255.0
+        let b = CGFloat((hex & 0x0000FF) >> 00) / 255.0
+
+        self.init(red: r, green: g, blue: b, alpha: alpha)
+    }
+}
